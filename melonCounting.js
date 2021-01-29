bf5ab815a166607520e9ec18ea728a59ebf7f247
Fionnie Pollack <fionniepollack@gmail.com>
2021-01-29 06:24:29 +0000

    Initial commit

---
+++ melonCounting.js
@@ -0,0 +1,27 @@
+const melons_to_add = ['Ogen', 'Horned Melon', 'Watermelon', 'Casaba',
+                 'Sharlyn', 'Xigua', 'Ogen', 'Christmas', 'Christmas',
+                 'Christmas', 'Christmas', 'Watermelon', 'Sharlyn', 'Xigua',
+                 'Cantaloupe', 'Christmas', 'Watermelon', 'Christmas',
+                 'Sharlyn', 'Christmas', 'Cantaloupe', 'Casaba', 'Cantaloupe',
+                 'Santa Claus', 'Horned Melon', 'Watermelon', 'Ogen',
+                 'Horned Melon', 'Cantaloupe', 'Xigua', 'Horned Melon', 'Sharlyn',
+                 'Horned Melon', 'Sharlyn', 'Cantaloupe', 'Christmas',
+                 'Horned Melon', 'Horned Melon', 'Horned Melon', 'Xigua', 'Xigua',
+                 'Watermelon', 'Cantaloupe', 'Casaba', 'Cantaloupe', 'Casaba',
+                 'Watermelon', 'Santa Claus', 'Casaba'];
+
+const countMelons = (melonList) => {
+    const melonCounts = {};
+    for (const melon of melonList) {
+        if (melonCounts.hasOwnProperty(melon)) {
+            melonCounts[melon] = (melonCounts[melon] + 1);
+        }
+        else {
+            melonCounts[melon] = 1;
+        }
+    }
+    return melonCounts
+}
+
+const result = countMelons(melons_to_add);
+console.log(result)
